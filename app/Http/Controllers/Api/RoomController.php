860dfca3414c1f25e1ed3e7655e1a99a36eb448b
joymozumder <joy.mozumder.1003@gmail.com>
2021-01-12 08:33:12 +0600

    room added

---
+++ app/Http/Controllers/Api/RoomController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Room;
+use App\Http\Resources\RoomResource;
+
+class RoomController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $rooms = Room::paginate(15);
+        return RoomResource::collection($rooms);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $room = new Room();
+        $room->number = $request->roomnumber;
+        $room->name = $request->roomname;
+        $room->type = $request->type;
+        $room->capacity = $request->capacity;
+        $room->status = $request->status;
+        if($room->save()){
+            return new RoomResource($room);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $room = Room::find($id);
+        return new RoomResource($room);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $room = Room::find($id);
+        return new RoomResource($room);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $room = Room::find($id);        
+        $room->name = $request->roomname;
+        $room->type = $request->type;
+        $room->capacity = $request->capacity;
+        $room->status = $request->status;
+
+        if($room->save()){
+            return new RoomResource($room);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $room = Room::find($id);
+        if($room->delete()){
+            return new RoomResource($room);
+        }
+    }
+}
+
---
+++ database/migrations/2021_01_12_020948_create_rooms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('number')->unique();
+            $table->string('name');
+            $table->integer('type')->comment('0=>Theory_Class, 1=>CSE_Lab, 2=>EEE_Lab, 3=>Communication_Lab, 4=>Mechanical_Lab, 5 => Physics_Lab');
+            $table->integer('capacity');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
