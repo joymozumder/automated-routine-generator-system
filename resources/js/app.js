13478521531e571a895c940d8f16cc759cd773c5
jitunmohajan <jitunmohajan@gmail.com>
2021-01-12 22:49:19 +0600

    vue initial setup with route  done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,56 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+// App.js
 
 require('./bootstrap');
 
 window.Vue = require('vue');
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
 
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+import VueAxios from 'vue-axios';
+import axios from 'axios';
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+import App from './App.vue';
+Vue.use(VueAxios, axios);
+import compExampleComponent from './components/ExampleComponent.vue';
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+const routes = [
+    {
+        name: 'home',
+        path: '/',
+        component: compExampleComponent
+    }
+  ];
 
-const app = new Vue({
-    el: '#app',
-});
+/* //this are like the basic setup file
+import compHomeComponent from './components/HomeComponent.vue';
+import compCreateComponent from './components/CreateComponent.vue';
+import compIndexComponent from './components/IndexComponent.vue';
+import compEditComponent from './components/EditComponent.vue';
+
+const routes = [
+ {
+     name: 'home',
+     path: '/',
+     component: compHomeComponent
+ },
+ {
+     name: 'create',
+     path: '/create',
+     component: compCreateComponent
+ },
+ {
+     name: 'posts',
+     path: '/posts',
+     component: compIndexComponent
+ },
+ {
+     name: 'edit',
+     path: '/edit/:id',
+     component: compEditComponent
+ }
+];
+*/
+
+const router = new VueRouter({ mode: 'history', routes: routes});
+const app = new Vue(Vue.util.extend({ router }, App)).$mount('#app');
