44085982bdcf34cfe8f45b181faa8622bfa2bd56
jitunmohajan <jitunmohajan@gmail.com>
2021-01-22 15:36:06 +0600

    adding table work

--- resources/js/components/Test.vue
+++ resources/js/components/Test.vue
@@ -1,5 +1,37 @@
 <template>
-	<div>
-		test
-	</div>
+	<vuetable ref="vuetable"
+    :api-mode="false"
+    :fields="['id', 'name', 'number', 'status','type']"
+    :data="rooms"
+    pagination-path=""
+  ></vuetable>
 </template>
+
+<script>
+import Vuetable from 'vuetable-2';
+import VuetablePagination from "vuetable-2/src/components/VuetablePagination";
+export default {
+	data(){
+		return{
+			rooms:[]
+		}		
+
+	},
+	created() {
+        let uri = '/api/rooms';
+        this.axios.get(uri).then(response => {
+          this.rooms = response.data.data;
+          console.log(this.rooms);
+          
+        });
+    },
+	components: {
+		Vuetable,
+		VuetablePagination
+	},
+	methods:{
+		
+	}
+	  
+}
+</script>
--- resources/js/components/Test2.vue
+++ resources/js/components/Test2.vue
@@ -10,14 +10,14 @@
                 <th slot="thead-tr">
                     Actions
                 </th>
-                <template slot="tbody-tr">
+                <template slot="tbody-tr" scope="props">
                     <td>
                          <button class="btn red darken-2 waves-effect waves-light compact-btn"
                             @click.prevent="deletePost(name)">
                             <i class="material-icons white-text">edit</i>
                         </button> 
                         <button class="btn red darken-2 waves-effect waves-light compact-btn"
-                            @click.prevent="deletePost(id)">
+                            @click.prevent="deletePost(props.row)"> 
                             <i class="material-icons white-text">delete</i>
                         </button>
                     </td>
@@ -90,13 +90,14 @@ export default {
             compDataTable 
      },
      methods: {
-      deletePost(id)
+      deletePost(row)
       {
-        console.log(id);
+        let id=row.id;
+        console.log(row);
         let uri = `/api/room/delete/${id}`;
         
         this.axios.delete(uri).then(response => {
-          this.rooms.splice(this.rooms.findIndex(room => room.id === id), 1);
+           this.rooms.splice(this.rooms.findIndex(room => room.id === id), 1);
         });
       }
     }
