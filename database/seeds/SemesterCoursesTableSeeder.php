3588824eba9ff944d22a388f29874fdfd49182aa
joymozumder <joy.mozumder.1003@gmail.com>
2021-02-05 12:37:10 +0600

    seeding updated

---
+++ database/seeds/SemesterCoursesTableSeeder.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\SemesterCourse;
+class SemesterCoursesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        SemesterCourse::create(['session_id'=>2,'course_id'=>1,'semester_section'=>'1A','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>2,'semester_section'=>'1A','group'=>1,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>2,'semester_section'=>'1A','group'=>2,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>3,'semester_section'=>'1A','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>4,'semester_section'=>'1A','group'=>1,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>4,'semester_section'=>'1A','group'=>2,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>5,'semester_section'=>'1A','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>6,'semester_section'=>'1A','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>7,'semester_section'=>'1A','group'=>1,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>7,'semester_section'=>'1A','group'=>2,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>8,'semester_section'=>'1A','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>9,'semester_section'=>'2A','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>10,'semester_section'=>'2A','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>11,'semester_section'=>'2A','group'=>1,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>11,'semester_section'=>'2A','group'=>2,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>12,'semester_section'=>'2A','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>13,'semester_section'=>'2A','group'=>1,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>13,'semester_section'=>'2A','group'=>2,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>14,'semester_section'=>'2A','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>15,'semester_section'=>'2A','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>16,'semester_section'=>'2A','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>9,'semester_section'=>'2B','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>10,'semester_section'=>'2B','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>11,'semester_section'=>'2B','group'=>1,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>11,'semester_section'=>'2B','group'=>2,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>12,'semester_section'=>'2B','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>13,'semester_section'=>'2B','group'=>1,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>13,'semester_section'=>'2B','group'=>2,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>14,'semester_section'=>'2B','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>15,'semester_section'=>'2B','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>16,'semester_section'=>'2B','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>9,'semester_section'=>'2C','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>10,'semester_section'=>'2C','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>11,'semester_section'=>'2C','group'=>1,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>11,'semester_section'=>'2C','group'=>2,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>12,'semester_section'=>'2C','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>13,'semester_section'=>'2C','group'=>1,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>13,'semester_section'=>'2C','group'=>2,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>14,'semester_section'=>'2C','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>15,'semester_section'=>'2C','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>16,'semester_section'=>'2C','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>17,'semester_section'=>'3A','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>18,'semester_section'=>'3A','group'=>1,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>18,'semester_section'=>'3A','group'=>2,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>19,'semester_section'=>'3A','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>20,'semester_section'=>'3A','group'=>1,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>20,'semester_section'=>'3A','group'=>2,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>21,'semester_section'=>'3A','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>22,'semester_section'=>'3A','group'=>0,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>23,'semester_section'=>'3A','group'=>1,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>23,'semester_section'=>'3A','group'=>2,'status'=>true,]);
+        SemesterCourse::create(['session_id'=>2,'course_id'=>24,'semester_section'=>'3A','group'=>0,'status'=>true,]);
+
+    }
+}
+//id	session_id	course_id	semester_section	group	status	created_at	updated_at
