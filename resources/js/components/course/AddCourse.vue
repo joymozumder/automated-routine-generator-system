0dab1a97acea2b42fb187374a7497fab728a1eae
jitunmohajan <jitunmohajan@gmail.com>
2021-02-19 08:35:23 +0600

    update

--- resources/js/components/course/AddCourse.vue
+++ resources/js/components/course/AddCourse.vue
@@ -96,7 +96,36 @@
                                         </div>
 
 
-
+                                        <div class="md:flex md:items-center mb-6">
+                                            <div class="md:w-1/4">
+                                                <label class="block text-gray-500 font-regular md:text-right mb-1 md:mb-0 pr-4"
+                                                    for="inline-course-code">
+                                                    Semester
+                                                </label>
+                                            </div>
+                                            <div class="md:w-3/4 relative">
+                                                <select class="block appearance-none w-full bg-grey-200 border border-grey-200 text-grey-darker py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-grey"
+                                                     v-model="course.semester"    id="grid-state">
+                                                        
+                                                        <option disabled value="-1">Select Semester</option>
+                                                        <option value="1">1st Semester</option>
+                                                        <option value="2">2nd Semester</option>
+                                                        <option value="3">3rd Semester</option>
+                                                        <option value="4">4th Semester</option>
+                                                        <option value="5">5th Semester</option>
+                                                        <option value="6">6th Semester</option>
+                                                        <option value="7">7th Semester</option>
+                                                        <option value="8">8th Semester</option>
+                                                        
+                                                    </select>
+                                                    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-grey-darker">
+                                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                                            viewBox="0 0 20 20">
+                                                            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
+                                                        </svg>
+                                                    </div>
+                                            </div>
+                                        </div>
                                   
 
                                         
@@ -133,6 +162,7 @@
                 code:"",
                 name:"",
                 credit:"-1",
+                semester:"-1",
                 type:-1,
                 status:false
           }
